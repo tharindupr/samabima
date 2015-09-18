21433425669e2dff6aa873aa1693700e5c704768
dmx9600 <d21max@gmail.com>
2015-09-19 04:07:33 +0530

    Main Page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,15 @@ angular.module('talkApp')
       'Karma'
     ];
 
+    $scope.images = [1, 2,3];
+
+    $scope.loadMore = function() {
+      var last = $scope.images[$scope.images.length - 1];
+      for(var i = 1; i <= 3; i++) {
+        $scope.images.push(last + i);
+      }
+    };
+
     //notificationService.talk
   });
 
