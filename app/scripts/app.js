96e3a7e1539a0ec104c8c65ffe3345ac4909ba11
tharindupr <tharindu.prf@gmail.com>
2015-09-19 08:57:56 +0530

    feed scrolling done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,35 +22,6 @@ var routerApp = angular.module('talkApp', [
     'infinite-scroll'
   ]);
 
-
-function Main($scope) {
-    $scope.items = [];
-
-    var counter = 0;
-    $scope.loadMore = function () {
-        for (var i = 0; i < 5; i++) {
-            $scope.items.push({
-                id: counter
-            });
-            counter += 10;
-        }
-    };
-
-    $scope.loadMore();
-}
-
-routerApp.directive('whenScrolled', function () {
-    return function (scope, elm, attr) {
-        var raw = elm[0];
-
-        elm.bind('scroll', function () {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-});
-
 routerApp.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
