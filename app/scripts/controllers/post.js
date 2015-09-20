b737f9c00df2e677f3fcfe80d70badda6febd76e
tharindupr <tharindu.prf@gmail.com>
2015-09-20 10:27:50 +0530

    tags working

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -3,16 +3,30 @@ angular.module('talkApp')
   .controller('post', function ($scope,$http) {
 
 $scope.asda="sadsa";
-  
+$scope.tags=[];
+  $scope.loadTags = function(query) {
+            return [{text:'Enviroment'},{text:'Parliment'},{text:'Water'},{text:'Pollution'},{text:'Religion'},{text:'WaterBoard'},{text:'Law'}];
+          };
+$scope.ta=[]
+
+
+
+
 
     $scope.postdata = function() {
 
+      for(i in $scope.tags)
+        {
+       
+          $scope.ta[i]=$scope.tags[i]['text'];
+        }
+      
     
     var post = {
         title : $scope.title,
         content : $scope.message,
         is_anonymouse : $scope.anonymouse,
-        tags : ["Education","Violence","Religion"],
+        tags : $scope.ta ,
         post_type_id: 1,
         owner_id : 1
         //tags : $scope.title
@@ -21,7 +35,8 @@ $scope.asda="sadsa";
 
     var res = $http.post("http://192.168.0.108/samabima/Samabima/public/post", post);
     res.success(function(data, status, headers, config) {
-      console.log(data);
+      location.reload();
+      
     });
 
 
