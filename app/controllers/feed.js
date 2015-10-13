fd38640630820c5ded55502229bd125de762522e
tharindupr <tharindu.prf@gmail.com>
2015-10-13 21:58:52 +0530

    tags done

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -9,29 +9,27 @@ controller('FeedCtrl', function ($scope,$http) {
           $scope.json=data;
           $scope.tagss=new Array();
          
-         angular.forEach($scope.json, function(json){
-
-           
-
-           $http({
-                  method: 'GET',
-                  url: 'http://hexmatter.cloudapp.net/post/'+json['post_id']+'/tags'
-                  }).then(function successCallback(response) {
-                          
-                              console.log(response);
-                  }, 
-                  function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                  });
-
-          });
-
+       
 
     });
 });
 
 
+app.controller('FeedTagCtrl',function($scope,$http){
+    
+     
+       
+       $http({
+            method: "GET",
+            url: "http://hexmatter.cloudapp.net/post/"+$scope.post['post_id']+"/tags"}).success(function(data, status, header, config){
+
+                    delete data['post_id'];
+                    $scope.tag=data;
+
+            });
+
+
+});
   
   
 
